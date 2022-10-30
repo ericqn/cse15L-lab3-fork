895eab8f40f047237b73406c3bedf710533baf3d
ericqn <ericqn1234@gmail.com>
2022-10-29 22:07:43 -0700

    added sample file structure

---
+++ FileTests.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileTests {
+    @Test 
+	public void testFilter() {
+        
+        assertEquals(true, true);
+	}
+}
